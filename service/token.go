3692d200427bc42f83b893f193a99f889580ae89
Makishi <makishi00@gmail.com>
2018-04-08 19:07:12 +0900

    loginTokenをsoft deleteするfuncの作成

--- service/token.go
+++ service/token.go
@@ -2,6 +2,7 @@ package service
 
 import (
 "github.com/makishi00/go-vue-bbs/model"
+"github.com/makki0205/gojwt"
 )
 
 
@@ -13,4 +14,18 @@ type token struct {
 func (t *token) Store(token model.Token) model.Token {
 	db.Create(&token)
 	return token
+}
+func (t *token) ExisByToken(token string) bool {
+	var tokens []model.Token
+	payload, err := jwt.Decode(token)
+	if err != nil{
+		panic(err)
+	}
+	db.Where("user_id = ?", payload["id"]).Find(&tokens)
+	return len(tokens) != 0
+}
+func (t *token) DeleteByUserId(userId int) bool {
+	var tokens []model.Token
+	db.Where("user_id = ?", userId).Delete(&tokens)
+	return len(tokens) != 0
 }
