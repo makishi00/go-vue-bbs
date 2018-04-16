3033c88882f6c6c50a4ecd61a4f7583ba6930492
Makishi <makishi00@gmail.com>
2018-04-16 17:09:02 +0900

    BBSへの投稿のサーバーでけた

---
+++ service/article.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"github.com/makishi00/go-vue-bbs/model"
+)
+
+
+var Article = article{}
+
+type article struct {
+}
+
+func (a *article) Store(article model.Article, userId uint) model.Article {
+	article.UserID = userId
+	db.Create(&article)
+	return article
+}
