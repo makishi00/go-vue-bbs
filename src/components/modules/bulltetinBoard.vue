e6dffbbb545b8fbaa051ddf6f847f5e17073ad8d
cheshirehat <cheshireinhatter@gmail.com>
2018-05-18 13:38:57 +0900

    BBSを表示できるようにフロント書いた

--- src/components/modules/bulltetinBoard.vue
+++ src/components/modules/bulltetinBoard.vue
@@ -12,27 +12,47 @@
         <label class="label">コメント</label>
         <textarea class="textarea"></textarea>
         <button class="button is-link">Submit</button>
+        <ul class="article-box">
+            <li v-for="data in articleItems">
+                タイトル:{{data.title}}<br>
+                本文:{{data.body}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-export default {
-  data () {
-      return {
-        items: [
-            {name: "編集", url: "", isActive: false},
-            {name: "削除", url: "", isActive: false}
-        ]
-    }
-  },
-  methods: {
-        select(e) {
-            for(let item of this.items) {
-                item.isActive = e.target.textContent === item.name
+    import article from '../../service/article'
+    import auth from"../../service/auth"
+
+    export default {
+        data () {
+            return {
+                items: [
+                    {name: "編集", url: "", isActive: false},
+                    {name: "削除", url: "", isActive: false}
+                ],
+                articleItems: []
             }
+        },
+        methods: {
+            async show() {
+                if (auth.getLoginToken() !== null) {
+                    const articleData = await article.show(auth.getLoginToken())
+                    articleData.data.forEach((data) => this.articleItems.push(data))
+                }
+                console.log(this.articleItems)
+            },
+            select(e) {
+                for(let item of this.items) {
+                    item.isActive = e.target.textContent === item.name
+                }
+            },
+        },
+        created () {
+            this.show()
         }
     }
-}
 </script>
 
 <style>
@@ -40,5 +60,11 @@ div.field {
     width: 80%;
     margin-left: 2.5%;
     float: left;
+}
+ul.article-box {
+    margin-top: 20px;
+}
+ul.article-box li {
+    margin-bottom: 10px;
 }
 </style>
